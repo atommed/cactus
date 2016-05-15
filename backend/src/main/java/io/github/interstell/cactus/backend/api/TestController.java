1e7f2f1d8a85f1434f44c6dddf72a2b85d98eda8
Gregory K <xalvevs@gmail.com>
2016-05-15 06:22:14 +0300

    Parse JSON

--- backend/src/main/java/io/github/interstell/cactus/backend/api/TestController.java
+++ backend/src/main/java/io/github/interstell/cactus/backend/api/TestController.java
@@ -4,11 +4,18 @@ import io.github.interstell.cactus.backend.models.Event;
 import io.github.interstell.cactus.backend.models.EventRowMapper;
 import io.github.interstell.cactus.backend.util.GoogleGeocode.LocationFinder;
 import io.github.interstell.cactus.backend.util.GooglePlaces.PlacesFinder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -55,6 +62,35 @@ public class TestController {
                 " order by -likes limit 10", new EventRowMapper());
 
         return res;
+    }
+
+    @RequestMapping("/fillDB")
+    @ResponseBody
+    public String fillDB(){
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject root = (JSONObject) parser.parse(new FileReader("C:\\Users\\Grigoriy\\Documents\\cactus\\backend\\src\\main\\resources\\test.json"));
+            JSONArray resp = (JSONArray) root.get("response");
+
+            for(int i = 1; i < resp.size(); i++){
+                JSONObject res = (JSONObject) resp.get(i);
+                JSONObject attachments = (JSONObject) res.get("attachment");
+                JSONObject photo = (JSONObject) attachments.get("photo");
+                String text = res.get("text").toString();
+                String img_uri = photo.get("src_big").toString();
+
+                System.out.println(img_uri);
+            }
+
+            System.out.println("Good");
+            return Integer.toString(resp.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 
     @RequestMapping("/k")
