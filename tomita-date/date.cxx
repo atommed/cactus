c6c515b19a5ebe742e6b27e5355a7adb2d135f21
samokhvalenko <ctac180898@gmail.com>
2016-05-15 03:43:58 +0300

    Need to be built together

---
+++ tomita-date/date.cxx
@@ -0,0 +1,23 @@
+﻿#encoding "utf8"
+
+DateS -> 'когда'Colon | 'коли'Colon | 'дата проведения' | 'Время' | 'января' | 'февраля' | 'марта' | 'апреля' | 'мая' | 'июня' | 'июля' | 'августа' | 'сентября' | 'октября' | 'ноября' | 'декабря';
+
+Starting -> 'начало' | 'початок' | 'старт' | 'початок' | 'начинается';
+
+DateSS -> DateS;
+
+DateOnlyDays -> (AnyWord<wff=/[0-3][0-9]-[0-3][0-9]/>) Word;
+
+DateMain -> (Word) (AnyWord<wff=/[0-3][0-9]/>) Word;
+
+DateTimeOnly -> DateMain ("время") (Colon) (Word) ("з") ("с") AnyWord<wff=/[0-2][0-9]:[0-5][0-9]/> ("по") ("до") (Hyphen) (Word) AnyWord<wff=/[0-2][0-9]:[0-5][0-9]/>;
+
+DateToDT -> Word ("сегодня") ("завтра") ("послезавтра") (Comma) (AnyWord<wff=/[0-2][0-9]:[0-6][0-9]/>);
+
+DateStupidForm -> (Word) (Comma) Starting (Prep) (AnyWord<wff=/[0-2][0-9]:[0-5][0-9]/>) (Word);
+
+DateMainT ->  DateOnlyDays | DateMain | DateTimeOnly | DateToDT | DateStupidForm;
+
+DateSM -> DateSS;
+
+Date -> DateSM interp (Date.prey) DateMainT interp (Date.Data);
