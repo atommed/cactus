ab95c00ba15e219a4ae6d05732779889e7397e42
samokhvalenko <ctac180898@gmail.com>
2016-05-15 05:12:44 +0300

    Tomitaaaa, prices, places and date already parsed

--- tomita-place/address.cxx
+++ tomita-place/address.cxx
@@ -8,14 +8,16 @@ PlaceT -> StreetW | StreetSokr;
 
 PlaceTW -> PlaceT+ (Punct) Word;
 
-PlaceTWW -> PlaceT+ (Punct) Word (Comma) (Word) (Comma) (AnyWord<wff=/[1-9]?[0-9]?/>);
+PlaceTWW -> PlaceT+ (Punct) Word (Comma) (Word) (Comma) (AnyWord<wff=/[0-9]?[0-9]?/>) (AnyWord<wff=/[0-9]?[0-9]?-?[0-9]?/>);
 
 PlaceTWCA -> PlaceT+ Word PlaceT* (Comma) PlaceT* (Word);
 
-PlaceMazo -> PlaceT PlaceT Punct* Word Comma* Punct AnyWord<wff=/[1-9]?[0-9]?/>;
+PlaceOM -> PlaceT Word (AnyWord<wff=/[0-9]?[0-9]?-?[0-9]?/>);
 
-PlaceTWA -> PlaceT+ Word PlaceT* (Word) PlaceT* AnyWord<wff=/[1-9]?[0-9]?/>;
+PlaceMazo -> PlaceT PlaceT Punct* Word Comma* Punct AnyWord<wff=/[0-9]?[0-9]?/> (AnyWord<wff=/[0-9]?[0-9]?-?[0-9]?/>);
 
-PlaceMain ->  PlaceTW | PlaceTWW | PlaceTWCA | PlaceTWA | PlaceMazo;
+PlaceTWA -> PlaceT+ Word PlaceT* (Word) PlaceT* AnyWord<wff=/[0-9]?[0-9]?/> (AnyWord<wff=/[0-9]?[0-9]?-?[0-9]?/>);
+
+PlaceMain ->  PlaceTW | PlaceTWW | PlaceTWCA | PlaceTWA | PlaceMazo | PlaceOM;
 
 Place -> PlaceMain interp (Address.StreetName);
